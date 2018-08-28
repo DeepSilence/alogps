b74557b5cabcb0a5119309f25240030976d21327
Mikaël RESTOUX <m.restoux@groupeonepoint.com>
2018-08-28 17:18:46 +0200

    Init of the server

--- server.js
+++ server.js
@@ -1,24 +1,48 @@
 const http = require('http');
 const fs = require('fs');
 
+const port = process.env.PORT || 8000;
 
-// Loads the HTML file to be served
-fs.readFile('resources/index.html', function (err, html) {
-    if (err) {
+/**
+ *  Main class of the server
+ */
+class Server {
+
+  constructor() {
+    this.html = 'Not loaded yet';
+    this.loadHtml();
+  }
+
+  start(){
+    this.createServer();
+  }
+
+  // Loads the HTML file to be served
+  loadHtml() {
+    fs.readFile('resources/index.html', function(err, html) {
+      if (err) {
         throw err;
-    }
+      }
 
-    this.createServer(html);
-});
+      this.html = html;
+    }.bind(this));
+  }
 
 
-// Creates the server
-const server = http.createServer((req, res) => {
-    res.writeHeader(200, {"Content-Type": "text/html"});
-    res.write(html);
-    res.end();
-});
-server.on('clientError', (err, socket) => {
-  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
-});
-server.listen(8000);
+  // Creates the server
+  createServer(html) {
+    const server = http.createServer((req, res) => {
+      res.writeHeader(200, {
+        "Content-Type": "text/html"
+      });
+      res.write(this.html);
+      res.end();
+    });
+    server.on('clientError', (err, socket) => {
+      socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+    });
+    server.listen(port);
+  }
+}
+
+new Server().start();
